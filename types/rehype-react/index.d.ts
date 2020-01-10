9b49a0358e67aa5207f4b024a9f0537ce515e977
Rikpat <33869814+Rikpat@users.noreply.github.com>
2020-01-10 23:06:31 +0100

    [rehype-react] Updated to version 4.0/4.1
    Added definition for Fragment on options object. This is way overdue since it was added in July.

--- types/rehype-react/index.d.ts
+++ types/rehype-react/index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for rehype-react 3.1
+// Type definitions for rehype-react 4.1
 // Project: https://github.com/rhysd/rehype-react
 // Definitions by: Adrian Kremer <https://github.com/adriankremer>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
@@ -12,6 +12,7 @@ interface Options {
         [tagName: string]: React.ComponentType<any>;
     };
     prefix?: string;
+    Fragment?: React.ElementType;
 }
 
 declare class RehypeReact {
